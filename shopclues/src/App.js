ee0ea0aedba1ff3fa9d447f283b5979c478a3c89
Prashant Ahire <prashantahire488@gmail.com>
2022-04-21 08:06:49 +0530

    update
    update

--- shopclues/src/App.js
+++ shopclues/src/App.js
@@ -1,13 +1,13 @@
 
 import './App.css';
-import {Home} from "./components/Home";
 import {Navbar} from "./components/Navbar";
+import {Product} from "./components/Product"
 
 function App() {
   return (
     <div className="App">
-    
       <Navbar/>
+      <Product/>
     </div>
   );
 }
--- shopclues/src/components/Product.jsx
+++ shopclues/src/components/Product.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 
-const Product = () => {
+function Product() {
+
+  const [user,setUser]=useState([]);
+
+  const fetchData=()=>
+  {
+    fetch("http://localhost:3001/data").then((res)=>
+    {
+      return res;
+    }).then((data)=>
+    {
+      console.log(data);
+    });
+  }
+  useEffect(
+    ()=>
+    {
+      fetchData();
+    }
+  )
   return (
     <div>
-      
+      <h1>Products</h1>
     </div>
   )
 }
 
-export default Product
+export {Product} 
